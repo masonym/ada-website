15c86f5fb2320bc0b31b6fc0c027f1f062e4ce19
Mason Leitch <nosamleitch@gmail.com>
2024-08-25 19:49:16 -0700

    Format fixing

--- src/app/events/[slug]/EventNavBar.tsx
+++ src/app/events/[slug]/EventNavBar.tsx
@@ -5,15 +5,15 @@ import { EVENTS } from '@/constants/events';
 import Link from 'next/link';
 import { notFound, useParams } from 'next/navigation';
 import { useRef, useState } from 'react';
-import { Menu } from 'lucide-react'; // Import the Menu icon from lucide-react
+import { Menu } from 'lucide-react';
 import Button from '@/app/components/Button';
 
 export default function Navbar() {
-    const params = useParams(); // Get the dynamic slug
-    const event = EVENTS.find(event => event.slug === params?.slug); // Find the event ID based on the slug
+    const params = useParams();
+    const event = EVENTS.find(event => event.slug === params?.slug);
     const [isDropdownOpen, setIsDropdownOpen] = useState(false);
     const [dropdownIndex, setDropdownIndex] = useState<number | null>(null);
-    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false); // State for mobile menu
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
     const timeoutRef = useRef<NodeJS.Timeout | null>(null);
 
     const handleMouseEnter = (index: number) => {
@@ -28,7 +28,7 @@ export default function Navbar() {
         timeoutRef.current = setTimeout(() => {
             setIsDropdownOpen(false);
             setDropdownIndex(null);
-        }, 200); // Adjust the delay as needed
+        }, 200);
     };
 
     if (!event) {
@@ -43,35 +43,25 @@ export default function Navbar() {
 
     return (
         <nav className="text-navy-800 text-[24px] my-0 p-4">
-            {/* Hamburger Icon for Mobile */}
             <div className="md:hidden flex items-center justify-between">
                 <button onClick={toggleMobileMenu} className="focus:outline-none">
-                    {/* Lucide Menu Icon */}
-                    <Menu className=" text-navy-800" size={64} />
+                    <Menu className="text-navy-800" size={64} />
                 </button>
             </div>
 
-            {/* Mobile Menu */}
             {isMobileMenuOpen && (
                 <ul className="md:hidden flex flex-col bg-gray-700 pl-8 rounded-md shadow-lg absolute left-0 right-0 z-10 mx-4 text-[16px]">
                     {params?.slug && navItems.map((navItem, index) => (
                         <li key={index} className="text-white p-2 rounded-md font-gotham">
                             {navItem.subItems ? (
                                 <>
-                                    {navItem.path && (
-                                        <Link
-                                            href={`/events/${params.slug}/${navItem.path}`}
-                                            className="block hover:bg-lightBlue-400 transition-colors duration-300 p-2 rounded-md"
-                                        >
-                                            {navItem.label}
-                                        </Link>
-                                    )}
+                                    <span className="block p-2 text-white">{navItem.label}</span>
                                     <ul className="mt-2 text-[14px]">
                                         {navItem.subItems.map(subItem => (
                                             <li key={subItem.path}>
                                                 <Link
                                                     href={`/events/${params.slug}/about/${subItem.path}`}
-                                                    className="block hover:bg-gray-600 transition-colors duration-300 rounded-md font-gotham  px-4 py-2 text-white"
+                                                    className="block hover:bg-gray-600 transition-colors duration-300 rounded-md font-gotham px-4 py-2 text-white"
                                                 >
                                                     {subItem.label}
                                                 </Link>
@@ -79,15 +69,13 @@ export default function Navbar() {
                                         ))}
                                     </ul>
                                 </>
-                            ) : navItem.path ? ( // Only render Link if path exists
+                            ) : (
                                 <Link
                                     href={`/events/${params.slug}/${navItem.path}`}
                                     className="block hover:bg-lightBlue-400 transition-colors duration-300 p-2 rounded-md"
                                 >
                                     {navItem.label}
                                 </Link>
-                            ) : (
-                                <span className="block p-2 text-gray-400">{navItem.label}</span> // Fallback text
                             )}
                         </li>
                     ))}
@@ -101,11 +89,8 @@ export default function Navbar() {
                 </ul>
             )}
 
-            {/* Desktop Menu */}
             <ul className="hidden md:flex relative items-center justify-center list-none">
-                {/* Centered Nav Items */}
                 <div className="flex items-center">
-                    {/* Optional space for additional elements */}
                     <li className="relative p-2 flex grow" />
                     {params?.slug && navItems.map((navItem, index) => (
                         <li
@@ -116,18 +101,9 @@ export default function Navbar() {
                         >
                             {navItem.subItems ? (
                                 <>
-                                    {navItem.path ? (
-                                        <Link
-                                            href={`/events/${params.slug}/${navItem.path}`}
-                                            className="hover:bg-lightBlue-400 hover:text-white transition-colors duration-300 p-2 px-4 rounded-full"
-                                        >
-                                            {navItem.label}
-                                        </Link>
-                                    ) : (
-                                        <span className="hover:bg-lightBlue-400 hover:text-white transition-colors duration-300 p-2 px-4 rounded-full text-navy-800">
-                                            {navItem.label} {/* Fallback text for items without a path */}
-                                        </span>
-                                    )}
+                                    <span className="hover:bg-lightBlue-400 hover:text-white transition-colors duration-300 p-2 px-4 rounded-full cursor-pointer">
+                                        {navItem.label}
+                                    </span>
                                     {isDropdownOpen && dropdownIndex === index && (
                                         <ul className="mt-4 absolute left-1/2 -translate-x-1/2 bg-gray-700 rounded-md shadow-lg list-none whitespace-nowrap">
                                             {navItem.subItems.map(subItem => (
@@ -143,24 +119,19 @@ export default function Navbar() {
                                         </ul>
                                     )}
                                 </>
-                            ) : navItem.path ? ( // Only render Link if path exists
+                            ) : (
                                 <Link
                                     href={`/events/${params.slug}/${navItem.path}`}
                                     className="hover:bg-lightBlue-400 hover:text-white transition-colors duration-300 p-2 px-4 rounded-full"
                                 >
                                     {navItem.label}
                                 </Link>
-                            ) : (
-                                <span className="hover:bg-lightBlue-400 hover:text-white transition-colors duration-300 p-2 px-4 rounded-full text-navy-800">
-                                    {navItem.label}  {/* Fallback text for items without a path */}
-                                </span>
                             )}
                         </li>
                     ))}
                     <li className="relative p-2 flex grow" />
                 </div>
 
-                {/* Register Button Aligned Right */}
                 <div className="flex justify-end">
                     <li className="relative p-2 flex grow max-w-[840px]">
                         <Button
