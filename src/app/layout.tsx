a1345eeb18d7ecafc199b5cd753ca6d58c0c5bd7
Mason Leitch <nosamleitch@gmail.com>
2024-08-27 10:25:35 -0700

    Added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata: Metadata = {
   title: "American Defense Alliance",
@@ -19,12 +19,13 @@ export default function RootLayout({
     <html lang="en">
       <body className="min-h-screen bg-gradient-to-b from-slate-50 to-slate-300">
         <div className="bg-navy-800">
-        <NavBar />
+          <NavBar />
         </div>
-        <SpeedInsights/>
         <main className="relative overflow-hidden ">
           {children}
         </main>
+        <SpeedInsights />
+        <Analytics />
         <Footer />
       </body>
     </html>
