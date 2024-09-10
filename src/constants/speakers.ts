a148f93f8d80fb591c4559166a057b063851da06
Mason Leitch <nosamleitch@gmail.com>
2024-09-10 13:10:58 -0700

    Added Brian Liesveld as a speaker

--- src/constants/speakers.ts
+++ src/constants/speakers.ts
@@ -13,6 +13,19 @@ export const SPEAKERS = [
                 He has an M.A. from the Fletcher School of Law & Diplomacy (Tufts & Harvard), and an A.B. from Princeton University/Woodrow Wilson School of Public & Int’l Affairs.
                 `
             },
+            {
+                image: "/speakers/brian_liesveld.jpg",
+                name: "Brian Liesveld",
+                position: "Chief Executive Officer",
+                company: "DEFENSEWERX",
+                bio: `
+                Brian Liesveld currently serves as the Chief Executive Officer (CEO) for DEFENSEWERX, Inc.  In his capacity as CEO, Brian oversees operations and management of the DWX innovation hubs as well as executing Board of Director’s strategic vision.
+                <br><br>
+                Prior to his role as Chief Executive Officer, Brian served as the Vice President for the Cyber Innovation Center in Bossier City, LA.  In this role, Brian oversaw the operations supporting the Air Force Global Strike Command as well as Cyber Curriculum delivered to over 3 million students across the United States.
+                <br><br>
+                Brian’s previous roles include the Associate Director of the Defense and Security Research Institute at the University of Arizona as well as multiple roles within the Department of Defense.  Most notably, Brian spearheaded the creation of the DoDs first University Affiliated Research Center (UARC) to be established in more than 20 years. 
+                `
+            }
         ]
     }
 ];
