27a7d4b30fea794040e0636ad668e2cce3c5569d
Mason Leitch <nosamleitch@gmail.com>
2024-09-11 17:00:59 -0700

    Updated speakers

--- src/constants/speakers.ts
+++ src/constants/speakers.ts
@@ -3,14 +3,41 @@ export const SPEAKERS = [
         id: 1,
         speakers: [
             {
-                image: "/speakers/charles_sills.png",
-                name: "Charles Sills",
-                position: "President & CEO",
-                company: "American Defense Alliance",
+                image: "/speakers/joachim_christian.png",
+                name: "His Royal Highness Prince Joachim",
+                position: "Prince of Denmark",
+                company: "",
                 bio: `
-                Charles Sills is a recognized authority on U.S. Government Contracting and an advocate for Small Business access to Federal and Military procurement. He is President of FED/Contracting, LLC, a Washington D.C. based consultancy that assists Small Businesses to succeed in the Government Sector, helps Prime Contractors qualify Diversity Suppliers, and facilitates Federal ‘Mentor-Protégé’ partnerships. He is a former Naval Intelligence Officer who served in National Security positions in the Pentagon and the Middle East/Indian Ocean Theater. Later he directed Defense Systems and Renewable Energy Strategic Marketing in Lockheed Martin’s International Division.
-                <br></br>
-                He has an M.A. from the Fletcher School of Law & Diplomacy (Tufts & Harvard), and an A.B. from Princeton University/Woodrow Wilson School of Public & Int’l Affairs.
+                Joachim Holger Waldemar Christian, Prince of Denmark, Count of Monpezat, was born on 7 June 1969. His Royal Highness Prince Joachim is the son of HM Queen Margrethe II and Prince Henrik of Denmark (d. 2018). He is included in the order of succession to the Throne and may act as Regent when HM The Queen and HRH Crown Prince Frederik are abroad.
+                <br><br>
+                Prince Joachim’s military career started in 1987 as recruit in the Queen's Own Regiment. In 1988, Prince Joachim was appointed as a sergeant, and in 1989, he was appointed as a lieutenant of the reserve. After becoming a lieutenant, Prince Joachim served as a Platoon Commander of a tank squadron in the Prince's Own Regiment from 1989 to 1990.
+                In 1990, Prince Joachim was appointed a first lieutenant of the reserve and in 1992, after completion of advanced training level I for officers of the reserve (VUT I), came his appointment as a captain of the reserve. From 1996 to 2004, Prince Joachim was the commander of a tank squadron in the mobilization force of the Prince's Own Regiment and, from 2005, was attached to the Danish Division’s staff as a major of the reserve.
+                Following appointment as a lieutenant colonel of the reserve in 2011, Prince Joachim was designated as the chief of the Liaison Officers Group in Total Defence Region Fyn, Syd and Sønderjylland.
+                From November 2015 to June 2019 Prince Joachim served as special advisor to the Danish Chief of Defence in relation to the implementation of the new total force concept, focusing on the Home Guard and personnel of the Reserve.
+                From August 2019 to June 2020 Prince Joachim attended the 69th session of the Center for Advanced Military Studies (CHEM) and the 72nd session of the Institute of Advanced National Defense Studies (IHEDN) at École Militaire in Paris.
+                Since September 2020 Prince Joachim is Defense Attache at the Royal Danish Embassy in Paris.
+                `
+            },
+            {
+                image: "/speakers/dave_leinberger.png",
+                name: "Dave Leinberger",
+                position: "Chief of Army Community Partnerships",
+                company: "ODCS G-9 (Installations), HQDA, Pentagon",
+                bio: 
+                `
+                Dave Leinberger assumed the role as the Chief of Army Community Partnerships in July 2024.
+                In this position, Mr. Leinberger encourages innovation and collaboration between our
+                installations and local communities to develop partnerships that are mutually beneficial,
+                identify cost efficiencies, reduce installation service response times, and strengthens the
+                relationship with the local communities outside our installations. Mr. Leinberger focuses the
+                utilization of collaborative partnership authorities to achieve these goals. These authorities
+                include Intergovernmental Support Agreements (IGSAs), Cooperative Agreements, Mutual Aid
+                Agreements, Memorandum of Understanding, Enhanced Use Lease, and Other Transaction
+                Authority.
+                <br><br>
+                Previously, Mr. Leinberger served as the Army Community Partnership Program Manager,
+                Assistant Chief of Staff for Installation Management, in September 2019 and maintained that
+                role following the ACSIM’s transition to Deputy Chief of Staff, G-9 (Installations).
                 `
             },
             {
@@ -25,7 +52,26 @@ export const SPEAKERS = [
                 <br><br>
                 Brian’s previous roles include the Associate Director of the Defense and Security Research Institute at the University of Arizona as well as multiple roles within the Department of Defense.  Most notably, Brian spearheaded the creation of the DoDs first University Affiliated Research Center (UARC) to be established in more than 20 years. 
                 `
-            }
+            },
+            {
+                image: "/speakers/charles_sills.png",
+                name: "Charles Sills",
+                position: "President & CEO",
+                company: "American Defense Alliance",
+                bio: `
+                Charles Sills is a recognized authority on U.S. Government Contracting and an advocate for Small Business access to Federal and Military procurement. He is President of FED/Contracting, LLC, a Washington D.C. based consultancy that assists Small Businesses to succeed in the Government Sector, helps Prime Contractors qualify Diversity Suppliers, and facilitates Federal ‘Mentor-Protégé’ partnerships. He is a former Naval Intelligence Officer who served in National Security positions in the Pentagon and the Middle East/Indian Ocean Theater. Later he directed Defense Systems and Renewable Energy Strategic Marketing in Lockheed Martin’s International Division.
+                <br></br>
+                He has an M.A. from the Fletcher School of Law & Diplomacy (Tufts & Harvard), and an A.B. from Princeton University/Woodrow Wilson School of Public & Int’l Affairs.
+                `
+            },
+            // {
+            //     image: "",
+            //     name: "",
+            //     position: "",
+            //     company: "",
+            //     bio: ``
+            // },
+
         ]
     }
 ];
