f570b64d947b742105c4466f2e39b440149b14e8
Mason Leitch <nosamleitch@gmail.com>
2024-10-22 00:46:00 -0700

    Updated exhibit text

--- src/constants/exhibitors.ts
+++ src/constants/exhibitors.ts
@@ -12,7 +12,7 @@ export const EXHIBITOR_TYPES = [
                     { tagline: "Full Access", description: "Access to All Event Sessions" },
                     { tagline: "Resources", description: "Speaker Presentations and Materials" },
                     { tagline: "Documentation", description: "Digital/Physical Copies of Agenda" },
-                    { tagline: "Catering", description: "Breakfast & Buffet Lunch" },
+                    { tagline: "Dining", description: "Breakfast & Buffet Lunch" },
                     { tagline: "Media Access", description: "Access to Photos & Videos from Conference" },
                 ]
             },
@@ -27,11 +27,11 @@ export const EXHIBITOR_TYPES = [
                 perks: [
                     { tagline: "Team Access", description: "Registration for 2 People" },
                     { tagline: "Networking", description: "2 VIP Networking Reception Passes" },
-                    { tagline: "Display Space", description: "6' Display Table in General Session" },
+                    { tagline: "Exhibit Space", description: "6' Display Table in General Session" },
                     { tagline: "Full Access", description: "Access to All Event Sessions" },
                     { tagline: "Resources", description: "Speaker Presentations and Materials" },
                     { tagline: "Documentation", description: "Digital/Physical Copies of Agenda" },
-                    { tagline: "Catering", description: "Breakfast & Buffet Lunch" },
+                    { tagline: "Dining", description: "Breakfast & Buffet Lunch" },
                     { tagline: "Media Access", description: "Access to Photos & Videos from Conference" },
                 ]
             },
