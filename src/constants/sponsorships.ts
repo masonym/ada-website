efed6b0179a4df36f8e03b0c4cb3fa81691ed991
Mason Leitch <nosamleitch@gmail.com>
2024-10-22 18:49:22 -0700

    Updated silver sponsor for 2025SDPC

--- src/constants/sponsorships.ts
+++ src/constants/sponsorships.ts
@@ -50,7 +50,7 @@ export const SPONSORSHIP_TYPES = [
                 perks: [
                     { tagline: "Event Access", description: " (5) VIP Attendee Passes. Additional VIP Attendee Passes can be Purchased for $395 each" },
                     { tagline: "Speaking Opportunity", description: "15-Minute Speaking Session for a Representative of your Company" },
-                    { tagline: "Networking Luncheon Hosting", description: "Host of the Networking Lunch on Day 1 of the Conference. Exclusive Signage around Lunch Buffet" },
+                    { tagline: "VIP Networking Reception Hosting", description: "Exclusive Host of the VIP Networking Reception on Day 1 of the Conference from 5:00 PM - 7:00 PM." },
                     { tagline: "Exhibit Space", description: "8'x10' Exhibit Space Strategically Placed in Exhibit Hall/Foyer. 6' Exhibit Table and 2 Chairs" },
                     { tagline: "Matchmaking Host", description: "Invitation to Host a One-on-One Matchmaking Session" },
                     { tagline: "Featured Sponsor Email", description: "(1) Promotional Email sent to all Registered Attendees Pre-Conference" },
@@ -58,19 +58,20 @@ export const SPONSORSHIP_TYPES = [
                     { tagline: "Recognition", description: "Acknowledgement during Key Moments of the Conference" },
                     { tagline: "Media Coverage", description: "Inclusion in Press Releases, Promotional Materials, Social Media Mentions. Photographs and Videos of your Participation" },
                 ],
-                colour: "#ffbf00"
+                colour: "#ffaf00"
             },
             {
                 title: "Silver Sponsor",
                 cost: "$5,000",
                 perks: [
-                    { tagline: "Event Access", description: "(4) conference passes and (4) complimentary tickets to the VIP Networking Reception" },
-                    { tagline: "Speaking Opportunity", description: "A brief segment during the conference for a company principal" },
-                    { tagline: "Brand Visibility", description: "Prominent logo placement on conference materials and signage" },
-                    { tagline: "Luncheon Hosting", description: "Host of the <i>Networking Lunch</i>" },
-                    { tagline: "Exhibit Space", description: "Ability to host a company exhibit during the conference" },
-                    { tagline: "Recognition", description: "Acknowledgment during key moments of the conference, such as opening remarks" },
-                    { tagline: "Media Coverage", description: "Inclusion in select materials, social media mentions, and post-conference coverage" },
+                    { tagline: "Event Access", description: "(4) VIP Attendee Passes. Additional VIP Attendee Passes can be Purchased for $395 each" },
+                    { tagline: "Speaking Opportunity", description: "10-Minute Speaking Session for a Representative of your Company" },
+                    { tagline: "Networking Luncheon Hosting", description: "Host of the Networking Lunch on Day 1 of the Conference. Exclusive Signage around Lunch Buffet" },
+                    { tagline: "Exhibit Space", description: "8'x10' Exhibit Space Strategically Placed in Exhibit Hall/Foyer. 6' Exhibit Table and 2 Chairs" },
+                    { tagline: "Matchmaking Host", description: "Invitation to Host a One-on-One Matchmaking Session" },
+                    { tagline: "Brand Visibility", description: " Prominent Logo Placement on all Conference Materials, including Banners, Marketing Emails, Website, and Programs" },
+                    { tagline: "Recognition", description: "Acknowledgement during Key Moments of the Conference" },
+                    { tagline: "Media Coverage", description: "Inclusion in Promotional Materials, Social Media Mentions. Photographs and Videos of your Participation" },
                 ],
                 colour: "#C0C0C0"
             },
