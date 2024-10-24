16eaf5b0d4d9d643424e30d0d41bc4da8e0047bd
Mason Leitch <nosamleitch@gmail.com>
2024-10-24 13:24:36 -0700

    Updated FAQs and event image

--- src/constants/faqs.tsx
+++ src/constants/faqs.tsx
@@ -73,46 +73,46 @@ export const FAQs = [
                 `,
             },
 
-            // {
-            //     question: "Who should attend this event?",
-            //     answer: "Attendees will include senior defense industry executives, Government officials, program managers, contracting officers, small business program directors, and representatives from various Military branches, along with prime defense contractors and industry analysts.",
-            // },
-            // {
-            //     question: "How can I register for the event?",
-            //     answer: `You can register for the event online through the <a href="https://www.eventbrite.com/e/2025-defense-industry-forecast-tickets-997306910817 target="_blank" class="text-blue-600 hover:underline text-nowrap">official event website</a>. Early registration is encouraged due to limited seating availability.`,
-            // },
-            // {
-            //     question: "Is there a cost to attend the event?",
-            //     answer: "Yes, there is a registration fee to attend the event. Please refer to the registration page for detailed pricing information and any available discounts.",
-            // },
-            // {
-            //     question: "Will there be networking opportunities?",
-            //     answer: "Yes, the event will include designated networking sessions, allowing attendees to connect with industry peers, policymakers, and other professionals. As well as a special ticketed Networking Reception from 5:30 PM - 7:30 PM ($100 Add-On option).",
-            // },
-            // {
-            //     question: "What is the agenda for the event?",
-            //     answer: "You will find expected session topics within the event information on the event website. The detailed agenda will be provided to registered attendees closer to the event date and will include session times, speaker information, and other important details.",
-            // },
-            // {
-            //     question: "What is the dress code for the event?",
-            //     answer: "The dress code for the event is Business Attire or Uniform of the Day.",
-            // },
-            // {
-            //     question: "Will the speaker presentations be available for download?",
-            //     answer: "Yes, all speaker presentations will be available for download on the American Defense Alliance website following the conclusion of the event. Registered attendees will receive an email notification with the password once the materials are uploaded.",
-            // },
-            // {
-            //     question: "Is there a refund policy for the event?",
-            //     answer: `All event registrations are final, and refunds are not available under any circumstances.  We understand that the unexpected can occur, therefore, if you cannot attend, you may transfer your registration to another individual up to one week before the event by contacting us at <a href="mailto:info@americandefensealliance.org" class="text-blue-600 hover:underline text-nowrap">info@americandefensealliance.org</a>.`,
-            // },
-            // {
-            //     question: "Will photos or videos be taken at the event?",
-            //     answer: "Yes, photos and videos will be taken during the event for promotional and archival purposes. By attending, you consent to being photographed and recorded, with the understanding that these images and recordings may be used by the American Defense Alliance in future marketing, publications, or media. If you have any concerns, please contact us prior to the event.",
-            // },
-            // {
-            //     question: "Who can I contact for more information?",
-            //     answer: `For any additional questions or concerns, please contact our event support team at <a href="mailto:info@americandefensealliance.org" class="text-blue-600 hover:underline text-nowrap">info@americandefensealliance.org</a> or call (771) 474-1077.`,
-            // }
+            {
+                question: "Who should attend this event?",
+                answer: "Attendees will include senior defense industry executives, Government officials, program managers, contracting officers, small business program directors, and representatives from various Military branches, along with prime defense contractors and industry analysts.",
+            },
+            {
+                question: "How can I register for the event?",
+                answer: `You can register for the event online through the <a href="https://www.eventbrite.com/e/2025-defense-industry-forecast-tickets-997306910817 target="_blank" class="text-blue-600 hover:underline text-nowrap">official event website</a>. Early registration is encouraged due to limited seating availability.`,
+            },
+            {
+                question: "Is there a cost to attend the event?",
+                answer: "Yes, there is a registration fee to attend the event. Please refer to the registration page for detailed pricing information and any available discounts.",
+            },
+            {
+                question: "Will there be networking opportunities?",
+                answer: "Yes, the event will include designated networking sessions, allowing attendees to connect with industry peers, policymakers, and other professionals. As well as a special ticketed Networking Reception from 5:30 PM - 7:30 PM ($100 Add-On option).",
+            },
+            {
+                question: "What is the agenda for the event?",
+                answer: "You will find expected session topics within the event information on the event website. The detailed agenda will be provided to registered attendees closer to the event date and will include session times, speaker information, and other important details.",
+            },
+            {
+                question: "What is the dress code for the event?",
+                answer: "The dress code for the event is Business Attire or Uniform of the Day.",
+            },
+            {
+                question: "Will the speaker presentations be available for download?",
+                answer: "Yes, all speaker presentations will be available for download on the American Defense Alliance website following the conclusion of the event. Registered attendees will receive an email notification with the password once the materials are uploaded.",
+            },
+            {
+                question: "Is there a refund policy for the event?",
+                answer: `All event registrations are final, and refunds are not available under any circumstances.  We understand that the unexpected can occur, therefore, if you cannot attend, you may transfer your registration to another individual up to one week before the event by contacting us at <a href="mailto:info@americandefensealliance.org" class="text-blue-600 hover:underline text-nowrap">info@americandefensealliance.org</a>.`,
+            },
+            {
+                question: "Will photos or videos be taken at the event?",
+                answer: "Yes, photos and videos will be taken during the event for promotional and archival purposes. By attending, you consent to being photographed and recorded, with the understanding that these images and recordings may be used by the American Defense Alliance in future marketing, publications, or media. If you have any concerns, please contact us prior to the event.",
+            },
+            {
+                question: "Who can I contact for more information?",
+                answer: `For any additional questions or concerns, please contact our event support team at <a href="mailto:info@americandefensealliance.org" class="text-blue-600 hover:underline text-nowrap">info@americandefensealliance.org</a> or call (771) 474-1077.`,
+            }
             // Add more FAQs here...
         ]
     },
