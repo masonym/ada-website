db2f13b7149dab5c2ffc7dc63c58c1bda477bcd7
Mason Leitch <nosamleitch@gmail.com>
2024-10-24 22:48:04 -0700

    Updated layout of event page

--- src/app/events/[slug]/page.tsx
+++ src/app/events/[slug]/page.tsx
@@ -128,7 +128,7 @@ export default function EventPage({ params }: { params: { slug: string } }) {
 
             <RegistrationOptions event={event} />
 
-            <div className="mt-12 text-center flex flex-col items-center">
+            <div className="mt-0 text-center flex flex-col items-center">
               <p className="text-2xl text-navy-500 mb-6 text-center mx-8">Act Now and Secure your Seat at this Groundbreaking Conference!</p>
               <Button
                 title="REGISTER"
