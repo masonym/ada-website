0aaab5ca28affc019b1f757effc151224c0932a1
Mason Leitch <nosamleitch@gmail.com>
2024-10-31 14:43:51 -0700

    Updated sponsor prospectus button

--- src/app/components/SponsorProspectus.tsx
+++ src/app/components/SponsorProspectus.tsx
@@ -9,10 +9,10 @@ const SponsorProspectus = ({ event }: SponsorProps) => {
         <Link
             href={event.sponsorProspectusPath}
             target='_blank'
-            className="inline-flex justify-center items-center px-8 py-3 mb-4 max-w-sm sm:max-w-lg bg-blue-900 text-white rounded-full hover:bg-blue-950 transition-all duration-300"
+            className="inline-flex justify-center items-center px-6 py-3 mb-4 max-w-sm sm:max-w-lg bg-blue-900 text-white rounded-full hover:bg-blue-950 transition-all duration-300"
         >
             <span className="font-semibold text-center">
-                Download Sponsorship & Exhibitor Prospectus
+                View Sponsorship & Exhibitor Prospectus
             </span>
         </Link>
     )
