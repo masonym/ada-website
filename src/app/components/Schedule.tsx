cc638b84efe64267218dd9d823d5b46d853f1058
Mason Leitch <nosamleitch@gmail.com>
2024-11-05 16:57:35 -0800

    Updated event agenda subtext

--- src/app/components/Schedule.tsx
+++ src/app/components/Schedule.tsx
@@ -51,7 +51,7 @@ const ScheduleAtAGlance: React.FC<ScheduleAtAGlanceProps> = ({
     <div className="max-w-[90rem] mx-auto px-4 sm:px-6 lg:px-8 pb-8">
       <h1 className="text-[48px] font-gotham font-bold mb-4 text-slate-700 text-center">Event Agenda</h1>
       <p className="text-l font-bold text-center mb-8 text-slate-600">
-        This event schedule is still pending, please check back later as more information is added!
+        The Event Agenda is still pending, please check back later as more information is added!
       </p>
       <div className="bg-white shadow-md rounded-lg overflow-hidden">
         <div className="flex border-b">
