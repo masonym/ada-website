d2523229658ab6d720685c8c5602609b1ac191ca
Mason Leitch <nosamleitch@gmail.com>
2024-11-05 16:38:51 -0800

    Updated contact page

--- src/constants/events.tsx
+++ src/constants/events.tsx
@@ -1,5 +1,6 @@
 // app/events/[slug]/page.tsx
 
+import { Mail, Phone } from "lucide-react";
 import Image from "next/image";
 
 export const EVENTS = [
@@ -242,7 +243,7 @@ export const EVENTS = [
       sponsorSection: (
         <>
           <div className="mt-4">
-          <h2 className="text-2xl sm:text-3xl lg:text-4xl font-bold text-center font-gotham text-slate-700 mt-6 mb-2">
+            <h2 className="text-2xl sm:text-3xl lg:text-4xl font-bold text-center font-gotham text-slate-700 mt-6 mb-2">
               Sponsors and Event Partners
             </h2>
             <div className="flex justify-center items-center gap-8 max-w-full">
@@ -281,14 +282,29 @@ export const EVENTS = [
         </>
       ),
       customContactText: (
-        <>
-          <b>Contact:</b> Lana Corrigan, Meetings & Events Executive
-          <br />
-          Phone: (202) 256-3028 | Email:{' '}
-          <a href="mailto:lana@americandefensealliance.org" className="underline">
-            lana@americandefensealliance.org
-          </a>
-        </>
+        <div className="flex flex-col items-center space-y-4 bg-white rounded-lg shadow-lg p-8">
+          <h3 className="text-3xl font-bold">Contact Information</h3>
+          <div className="text-center">
+            <p className="text-xl font-bold">Lana Corrigan</p>
+          </div>
+
+          <div className="flex flex-col sm:flex-row items-center justify-center gap-6 w-full">
+            <div className="flex items-center gap-2">
+              <Phone className="w-5 h-5" />
+              <span>(202) 256-3028</span>
+            </div>
+
+            <div className="flex items-center gap-2">
+              <Mail className="w-5 h-5" />
+              <a
+                href="mailto:lana@americandefensealliance.org"
+                className="hover:text-blue-200 underline transition-colors duration-300"
+              >
+                lana@americandefensealliance.org
+              </a>
+            </div>
+          </div>
+        </div>
       )
     },
   }
