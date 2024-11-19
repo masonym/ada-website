1557d7e7d321a0748da4606c55e97c6323d44169
Mason Leitch <nosamleitch@gmail.com>
2024-11-18 20:14:53 -0800

    Update vertical padding event recap

--- src/app/events/[slug]/about/event-recap/HighlightedPhotos.tsx
+++ src/app/events/[slug]/about/event-recap/HighlightedPhotos.tsx
@@ -36,7 +36,7 @@ const HighlightedPhotos: React.FC<HighlightedPhotosProps> = ({ images }) => {
   return (
     <div className="rounded-lg overflow-hidden">
 
-      <div className="columns-1 sm:columns-2 lg:columns-3 py-10 md:py-20 gap-4 gap-y-5">
+      <div className="columns-1 sm:columns-2 lg:columns-3 py-2 md:py-4 gap-4 gap-y-5">
         {
           images.map((img, index) => (
             <div key={img.src} className="relative mb-4 break-inside-avoid">
