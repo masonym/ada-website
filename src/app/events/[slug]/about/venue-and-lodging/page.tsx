de1dabd4384abb20f5b666c9726edaf538b09b7e
Mason Leitch <nosamleitch@gmail.com>
2024-12-11 20:30:02 -0800

    Updated text

--- src/app/events/[slug]/about/venue-and-lodging/page.tsx
+++ src/app/events/[slug]/about/venue-and-lodging/page.tsx
@@ -56,7 +56,7 @@ export default function VenueAndLodgingPage({ params }: { params: { slug: string
                                             <MapPin className="w-5 h-5 mr-2 mt-1 flex-shrink-0 text-gray-400" />
                                             <div>
                                                 <p>{`${hotel.address}, ${hotel.city}, ${hotel.state} ${hotel.zip}`}</p>
-                                                <p>Crystal Ballroom</p>
+                                                <p>Crystal Ballroom First Floor</p>
                                             </div>
                                         </div>
                                         <div className="flex items-center">
