fba49517f8c1b8aaea2d7ab1f6753ab0363a45e1
Mason Leitch <nosamleitch@gmail.com>
2024-12-11 22:51:07 -0800

    Text update

--- src/app/events/[slug]/about/venue-and-lodging/page.tsx
+++ src/app/events/[slug]/about/venue-and-lodging/page.tsx
@@ -35,7 +35,7 @@ export default function VenueAndLodgingPage({ params }: { params: { slug: string
             {lodging && (
                 <section className="max-w-[86rem] mx-auto">
                     <h2 className="text-3xl sm:text-4xl lg:text-5xl font-bold text-center font-gotham text-slate-700 mb-8">
-                        Venue and Recommended Lodging
+                        Event Venue and Recommended Lodging
                     </h2>
 
                     <div className="grid grid-cols-1 md:grid-cols-1 gap-8 max-w-[50%] mx-auto">
