3d5e9529fb005cc579ed7e1ad6d29e9b3eda3fb3
Mason Leitch <nosamleitch@gmail.com>
2025-03-11 23:10:01 -0700

    Fix: Hydration error

--- src/app/print-schedule/layout.tsx
+++ src/app/print-schedule/layout.tsx
@@ -12,11 +12,6 @@ export default function PrintScheduleLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </head>
-      <body>{children}</body>
-    </html>
+    <div>{children}</div>
   );
 }
