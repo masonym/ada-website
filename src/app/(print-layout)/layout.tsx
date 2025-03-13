e38a5bf78ad7d6c5379cbb4cd4d31a9a1c533415
Mason Leitch <nosamleitch@gmail.com>
2025-03-12 22:31:21 -0700

    idk what happened

--- src/app/(print-layout)/layout.tsx
+++ src/app/(print-layout)/layout.tsx
@@ -1,22 +1,21 @@
 import React from 'react';
-import './print-styles.css';
+import '../print-schedule/print-styles.css';
 
 export const metadata = {
   title: 'Printable Schedule',
   description: 'Printable schedule for ADA events',
 };
 
-export default function PrintScheduleLayout({
+export default function PrintLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </head>
-      <body>{children}</body>
+      <body className="print-layout">
+        {children}
+      </body>
     </html>
   );
 }
