585371b1f5cb04f1c408aac4c5675516a6c489d6
Mason Leitch <nosamleitch@gmail.com>
2025-07-01 22:03:32 -0700

    2025nmcpc: updated text for vip reception

--- src/constants/events.tsx
+++ src/constants/events.tsx
@@ -804,7 +804,7 @@ invaluable connections that can shape the future of your business.
       timeStart: "6:00 PM",
       timeEnd: "8:00 PM",
       description: "The VIP Networking Reception is available to all Speakers, Sponsors, Exhibitors, VIP Attendee Passes, and Special Guests.",
-      additionalInfo: "Join us for the VIP Networking Reception at The Harbor Club featuring a cash bar, one complimentary drink ticket, and a selection of hors d’oeuvres.",
+      additionalInfo: "Join us at The Harbor Club featuring a cash bar, one complimentary drink ticket, and a selection of hors d’oeuvres.",
       locationName: "The Harbor Club",
       locationAddress: "333 Waterside Dr Suite 200, Norfolk, VA 23510, USA",
       placeId: "ChIJI8LspwuYuokR79KzVtIGqlY", // Google Maps Place ID for the reception venue
