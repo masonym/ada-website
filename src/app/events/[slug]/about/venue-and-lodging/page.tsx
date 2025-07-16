62bba9b685c014605bc631c275cfd90cb9a53f87
Mason Leitch <nosamleitch@gmail.com>
2025-07-16 12:05:20 -0700

    added reusable component for lodging section

--- src/app/events/[slug]/about/venue-and-lodging/page.tsx
+++ src/app/events/[slug]/about/venue-and-lodging/page.tsx
@@ -4,11 +4,12 @@ import React, { useState } from 'react';
 import { EVENTS } from '@/constants/events';
 import { notFound } from 'next/navigation';
 import Image from 'next/image';
-import { ChevronDown, ChevronRight, Globe, MapPin, Phone } from 'lucide-react';
+import { ChevronDown, ChevronRight } from 'lucide-react';
 import { LODGING_INFO } from '@/constants/lodging';
 import { getCdnPath } from '@/utils/image';
 import Map from '../venue/Map';
 import Link from 'next/link';
+import LodgingSection from '@/app/components/LodgingSection';
 
 export default function VenueAndLodgingPage({ params }: { params: { slug: string } }) {
     const event = EVENTS.find((e) => e.slug === params.slug);
@@ -33,66 +34,7 @@ export default function VenueAndLodgingPage({ params }: { params: { slug: string
             {/* Venue Section */}
 
             {/* Lodging Section */}
-            {lodging && (
-                <section className="max-w-[86rem] mx-auto">
-                    <h2 className="text-3xl sm:text-4xl lg:text-5xl font-bold text-center font-gotham text-slate-700 mb-8">
-                        Event Venue & Lodging
-                    </h2>
-
-                    <div className="grid grid-cols-1 md:grid-cols-1 max-w-[90%] md:max-w-[60%] gap-8 mx-auto">
-                        {lodging.hotels.map((hotel, index) => (
-                            <div key={index} className="bg-white rounded-lg shadow-md overflow-hidden">
-                                <div className="relative h-[30rem]">
-                                    <Image
-                                        src={getCdnPath(hotel.image)}
-                                        alt={hotel.name}
-                                        fill
-                                        className="object-cover object-[75%_15%]"
-                                    />
-                                </div>
-                                <div className="p-6">
-                                    <h3 className="text-2xl font-bold mb-4 text-navy-800">{hotel.name}</h3>
-                                    <div className="space-y-2 text-gray-600">
-                                        <div className="flex items-start">
-                                            <MapPin className="w-5 h-5 mr-2 mt-1 flex-shrink-0 text-gray-400" />
-                                            <div>
-                                                {/* only print address, city, state, if they are there */}
-                                                {/* if all exist, print address, city, state, zip on one line separated by commas */}
-                                                {/* use spans so that they appear on one line */}
-                                                {hotel.address && <span>{hotel.address}</span>}
-                                                {hotel.city && <span>, {hotel.city}</span>}
-                                                {hotel.state && <span>, {hotel.state}</span>}
-                                                {hotel.zip && <span> {hotel.zip}</span>}
-                                            </div>
-                                        </div>
-                                        <div className="flex items-center">
-                                            <Phone className="w-5 h-5 mr-2 flex-shrink-0 text-gray-400" />
-                                            <p>{hotel.phone}</p>
-                                        </div>
-                                        {hotel.link &&
-                                            <div className="flex items-center">
-                                                <Globe className="w-5 h-5 mr-2 flex-shrink-0 text-gray-400" />
-                                                <Link href={hotel.link.href} className=" text-blue-600 hover:underline" target='_blank'>
-                                                    <p className='my-2'>{hotel.link.label}</p>
-                                                </Link>
-                                            </div>
-                                        }
-                                        <p className="mt-8 text-left text-gray-600">
-                                            {/* If any of the address/state/city/zip are blank, don't display lodging note*/}
-                                            {hotel.city || hotel.state || hotel.zip ? (
-                                                <div className="flex flex-col">
-                                                    {lodging.note && <span dangerouslySetInnerHTML={{ __html: lodging.note }}></span>}
-                                                </div>
-                                            ) : null}
-                                        </p>
-
-                                    </div>
-                                </div>
-                            </div>
-                        ))}
-                    </div>
-                </section>
-            )}
+            {lodging && <LodgingSection lodging={lodging} className="max-w-[46rem] mx-auto"/>}
 
             {/* Norfolk Dining Guide Section */}
             {event.id === 4 && (
