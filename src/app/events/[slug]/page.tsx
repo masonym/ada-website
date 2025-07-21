6174593b81f5431676f1c3e131e646c50b965628
Mason Leitch <nosamleitch@gmail.com>
2025-07-21 13:55:10 -0700

    feat: moved event warnign to faqs page

--- src/app/events/[slug]/page.tsx
+++ src/app/events/[slug]/page.tsx
@@ -14,7 +14,6 @@ import SpecialFeatures from '@/app/components/SpecialFeatures';
 import FooterEventText from '@/app/components/FooterEventText';
 import { EventSaleBanner } from '@/app/components/EventSaleBanner';
 import SponsorAdvert from '@/app/components/SponsorAdvert';
-import EventWarningNotice from '@/app/components/EventWarningNotice';
 
 export async function generateStaticParams() {
   return EVENTS.map((event) => ({
@@ -137,8 +136,6 @@ export default function EventPage({ params }: { params: { slug: string } }) {
 
 
             <SpecialFeatures event={event} />
-
-            <EventWarningNotice eventTitle={event.title}/>
 
             <RegistrationOptions event={event} />
 
