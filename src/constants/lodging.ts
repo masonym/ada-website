d380b9f7997593d5f1d8141b7663eddfd5cc86f8
Mason Leitch <nosamleitch@gmail.com>
2025-07-22 12:27:40 -0700

    lodging update

--- src/constants/lodging.ts
+++ src/constants/lodging.ts
@@ -111,11 +111,11 @@ export const LODGING_INFO: LodgingInfo[] = [
     <b>Rate Available</b>: November 3-8, 2025<br />
     <b>Booking Deadline</b>: October 21, 2025<br />
     <br/>
+    <a href="https://www.marriott.com/event-reservations/reservation-link.mi?id=1753131167945&key=GRP&app=resvlink" target="_blank" rel="noopener noreferrer" class="underline hover:text-accent text-blue-500">Reservation Link</a>
+    <br/>
+    <br/>
     <b>To Book Extra Nights at the Group Rate, please email <a href="mailto:kaitlin.cahill@renaissancehotels.com" class="text-blue-600 hover:underline text-nowrap">kaitlin.cahill@renaissancehotels.com</a></b>
     <b>Subject Line: "Request to Add Reservation at Group Rate" 2025 Defense Technology & Aerospace Procurement Conference</b>
-    <br/>
-    <br/>
-    <a href="https://www.marriott.com/event-reservations/reservation-link.mi?id=1753131167945&key=GRP&app=resvlink" target="_blank" rel="noopener noreferrer" class="underline hover:text-accent text-blue-500">Reservation Link</a>
     <br/>
     <br/>
     <p class="font-bold"> Parking: </p>
