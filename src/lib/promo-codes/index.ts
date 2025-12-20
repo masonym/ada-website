044295cfcadbca83dcec76ea67b5c244becaf3ac
Mason Leitch <nosamleitch@gmail.com>
2025-12-19 17:08:33 -0800

    remove promo code

--- src/lib/promo-codes/index.ts
+++ src/lib/promo-codes/index.ts
@@ -214,7 +214,7 @@ export const PROMO_CODES: PromoCode[] = [
     eligibleEventIds: [6], // 2026NMCPC
     expirationDate: new Date('2026-12-02T05:00:00Z'), // Dec 2, 2026 12:00 AM EST in UTC
     description: 'CSS25 - 25% off eligible passes and sponsorships for 2026 Navy & Marine Corps conference',
-    isActive: true
+    isActive: false
   },
   {
     code: 'CISEVE15',
