3a51d054b736f62f96876245bcaa65c331362858
Mason Leitch <nosamleitch@gmail.com>
2026-03-10 13:02:49 -0700

    update testimonial and metadata

--- src/constants/events.tsx
+++ src/constants/events.tsx
@@ -1092,7 +1092,6 @@ A discounted parking rate of $22.00/Car/Night is offered with in and out privile
       {
         type: 'image',
         quote: `I wanted to extend my sincere thanks to the American Defense Alliance team for the opportunity to participate in the Defense Technology & Aerospace Procurement Conference in Austin. It was a truly well-organized event, and I appreciated the chance to contribute to the conversation around cybersecurity readiness for Defense Industrial Base contractors and the resources available through Project Spectrum.
-        <br/>
         The conference brought together an outstanding mix of government leaders, primes, innovators, and small businesses, and it was a privilege to engage with so many members of the defense and aerospace ecosystem. Your team did an excellent job creating an environment where meaningful dialogue and collaboration could take place.`,
         name: "Kareem A. Sykes",
         title: "Project Spectrum, Industry & Engagement, Director",
