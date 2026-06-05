616abe0cf1e22a39a484776535b7a9395bccf163
Mason Leitch <nosamleitch@gmail.com>
2026-06-04 22:57:26 -0700

    add event metrics and update script

--- src/constants/eventMetrics.ts
+++ src/constants/eventMetrics.ts
@@ -42,6 +42,20 @@ export const EVENT_METRICS_CONFIGS: EventMetricsConfig[] = [
     organizationColumn: 'Company/Organization Name', // which column to use from the CSV for the organization breakdown
     excludedRegistrationTypes: ['Staff'], // registration types to exclude from the breakdown
   },
+  {
+    eventId: 6,
+    csvPath: 'events/2026NMCPC/registration-information.csv',
+    title: 'Event Metrics',
+    speakerCount: 38,
+    matchmakingHosts: 13,
+    // oneOnOneAppointments: 146,
+    industryColumn: 'Industry',
+    businessSizeColumn: 'Business Size',
+    roleColumn: 'Contact Title', // which column to use from the CSV for the role breakdown
+    registrationTypeColumn: 'Registration Type', // which column to use from the CSV for the registration type breakdown
+    organizationColumn: 'Company/Organization Name', // which column to use from the CSV for the organization breakdown
+    excludedRegistrationTypes: ['Staff'], // registration types to exclude from the breakdown
+  },
 ];
 
 export function getEventMetricsConfig(eventId: number): EventMetricsConfig | undefined {
