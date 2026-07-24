26676b03db415d148e36fc6f382ffb8f2ec67c6b
Mason Leitch <nosamleitch@gmail.com>
2026-07-23 21:47:18 -0700

    fix redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,52 +1,55 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        unoptimized: true, // temporary due to vercel limits
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'americandefensealliance.org',
-                port: '',
-            },
-            {
-                protocol: 'https',
-                hostname: 'd3hatd5vc8h86k.cloudfront.net',
-                port: '',
-            },
-            {
-                protocol: 'https',
-                hostname: 'cdn.americandefensealliance.org',
-                port: '',
-            },
-            {
-                protocol: 'https',
-                hostname: 'nc4xlou0.cdn.sanity.io',
-                port: '',
-            }
-        ],
-        deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840, 4096],
-        imageSizes: [16, 32, 48, 64, 96, 128, 256, 384, 512],
-    },
-    env: {
-        AWS_REGION: process.env.AWS_REGION,
-        AWS_S3_BUCKET_NAME: process.env.AWS_BUCKET_NAME,
-        AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
-        AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
-    },
-    async redirects() {
-        return [
-            {
-                source: '/events/2025-defense-technology-aerospace-procurement-conference/:path*',
-                destination: '/events/2026-defense-technology-aerospace-procurement-conference/:path*',
-                permanent: true,
-            },
-            {
-                source: '/events/:slug/sponsors-exhibitors/sponsorship-opportunities',
-                destination: '/events/:slug/sponsorships-exhibits',
-                permanent: true,
-            },
-        ];
-    },
+  images: {
+    unoptimized: true, // temporary due to vercel limits
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "americandefensealliance.org",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "d3hatd5vc8h86k.cloudfront.net",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.americandefensealliance.org",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "nc4xlou0.cdn.sanity.io",
+        port: "",
+      },
+    ],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840, 4096],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384, 512],
+  },
+  env: {
+    AWS_REGION: process.env.AWS_REGION,
+    AWS_S3_BUCKET_NAME: process.env.AWS_BUCKET_NAME,
+    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+  async redirects() {
+    return [
+      {
+        source:
+          "/events/2025-defense-technology-aerospace-procurement-conference/:path*",
+        destination:
+          "/events/2026-defense-technology-aerospace-procurement-conference/:path*",
+        permanent: true,
+      },
+      {
+        source:
+          "/events/:slug/sponsors-exhibitors/sponsorship-opportunities/:path*",
+        destination: "/events/:slug/sponsorships-exhibits/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
